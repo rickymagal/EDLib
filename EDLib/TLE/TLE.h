639945eea5bec88dda818650d14ee5467a831a10
rickmagal <rickmagal@debian>
2023-03-29 11:18:08 -0400

    TLE - Tipo Lista Encadeada

--- EDLib/TLE/TLE.h
+++ EDLib/TLE/TLE.h
@@ -1,16 +1,24 @@
 typedef struct lista Lista;
 
-Lista * lst_cria(void);
-Lista * lst_insere(Lista *l, int i);
-Lista * lst_retira(Lista  *l, int v);
-Lista * lst_busca(Lista *l, int v);
-Lista * lst_insere_ordenado(Lista *l, int v);
-Lista * lst_retira_rec(Lista * l, int v);
-
+Lista* lst_cria(void);
+Lista* lst_insere_ini(Lista *l, int i);
+Lista* lst_insere_fim(Lista* l, int elem);
+Lista* lst_insere_fim_rec(Lista* l, int elem);
+Lista* lst_retira_elem(Lista  *l, int v);
+Lista* lst_busca(Lista *l, int v);
+Lista* lst_busca_rec(Lista* l, int elem);
+Lista* lst_insere_ordenado(Lista *l, int v);
+Lista* lst_retira_elem_rec(Lista * l, int v);
+Lista* lst_retira_pos(Lista* l, int pos);
+Lista* retira_todas_oc (Lista* l, int elem);
+Lista* lst_insere_ordenado_rec(Lista* l, int elem);
+Lista* copia (Lista *l);
+Lista* inverte(Lista* l);
+Lista* desloca(Lista *l, int n);
 
 int lst_vazia(Lista * l);
 
 void lst_imprime(Lista *l);
 void lst_imprime_rec(Lista *l);
 void lst_libera(Lista *l);
-void lst_libera_rec(Lista *l);
+void inverte_modifica(Lista** l);
